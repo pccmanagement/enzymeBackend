84d7e9b676ffe4bc39c31c4a22b4f1f2735d84dc
Viraj Jain <harshal.jain.csai.20@ggits.net>
2024-07-24 04:16:54 +0530

    added company email and poster

--- db.js
+++ db.js
@@ -10,7 +10,7 @@ mongoose.connect(MONGO_URL, {
     dbName: DB_NAME
 }).then(
     () => {
-        console.log('Connected to database');
+        console.log('Connected to database ',MONGO_URL);
     }
 ).catch((err) => {
     console.log('Error connecting to database ' + err);
